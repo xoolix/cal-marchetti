bdde31fad0e7ccf578c09dc2f786375351b0bf61
Hariom Balhara <hariombalhara@gmail.com>
2022-04-08 14:56:13 +0530

    Add test to verify iframe communication

--- packages/embeds/embed-core/playwright/fixtures/fixtures.ts
+++ packages/embeds/embed-core/playwright/fixtures/fixtures.ts
@@ -1,3 +1,45 @@
-import { test as base } from "@playwright/test";
+import { test as base, Page } from "@playwright/test";
 
-export const test = base.extend({});
+interface Fixtures {
+  addEmbedListeners: (calNamespace: string) => Promise<void>;
+  getActionFiredDetails: (a: { calNamespace: string; actionType: string }) => Promise<any>;
+}
+export const test = base.extend<Fixtures>({
+  addEmbedListeners: async ({ page }: { page: Page }, use) => {
+    await use(async (calNamespace: string) => {
+      await page.addInitScript(
+        ({ calNamespace }: { calNamespace: string }) => {
+          //@ts-ignore
+          window.eventsFiredStoreForPlaywright = window.eventsFiredStoreForPlaywright || {};
+          document.addEventListener("DOMContentLoaded", () => {
+            console.log("PlaywrightTest:", "Adding listener for __iframeReady");
+            debugger;
+            //@ts-ignore
+            window.Cal.ns[calNamespace]("on", {
+              action: "*",
+              callback: (e: any) => {
+                //@ts-ignore
+                const store = window.eventsFiredStoreForPlaywright;
+                let eventStore = (store[`${e.detail.type}-${e.detail.namespace}`] =
+                  store[`${e.detail.type}-${e.detail.namespace}`] || []);
+                eventStore.push(e.detail);
+              },
+            });
+          });
+        },
+        { calNamespace }
+      );
+    });
+  },
+  getActionFiredDetails: async ({ page }, use) => {
+    await use(async ({ calNamespace, actionType }) => {
+      return await page.evaluate(
+        ({ actionType, calNamespace }) => {
+          //@ts-ignore
+          return window.eventsFiredStoreForPlaywright[`${actionType}-${calNamespace}`];
+        },
+        { actionType, calNamespace }
+      );
+    });
+  },
+});
