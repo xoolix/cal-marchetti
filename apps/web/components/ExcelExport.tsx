6f4052879da22fb40295dcf7af11e685ff376281
ocampott <ocampott7@gmail.com>
2022-06-06 19:06:52 -0300

    Added excel feature

---
+++ apps/web/components/ExcelExport.tsx
@@ -0,0 +1,46 @@
+import dayjs from "dayjs";
+import React from "react";
+import ReactExport from "react-export-excel-xlsx-fix";
+
+import { Button } from "@calcom/ui";
+
+export const ExcelExport = ({ booking }) => {
+  const ExcelFile = ReactExport.ExcelFile;
+  const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+  const excelData = [
+    {
+      columns: [
+        { value: "Paciente", widthCh: 150 },
+        { value: "Fecha y hora", widthPx: 100 },
+        { value: "Tipo de consulta", widthPx: 100 },
+      ],
+      data: [],
+    },
+  ];
+
+  const updateData = () => {
+    for (let i = 0; i < booking.length; i++) {
+      excelData[0].data.push([
+        booking[i].attendees[0].name,
+
+        dayjs(booking[i].startTime).format("DD-MM HH:mm"),
+
+        booking[i].eventType.eventName,
+      ]);
+    }
+  };
+
+  console.log(excelData);
+
+  return (
+    <ExcelFile
+      filename="Turnos"
+      element={
+        <Button onClick={updateData} className="mt-2 ml-2 mb-2" color="secondary">
+          Excel
+        </Button>
+      }>
+      <ExcelSheet dataSet={excelData} name="Turnos" />
+    </ExcelFile>
+  );
+};
