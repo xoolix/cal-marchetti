feaadcbb08ac1cb72ae22623a517cb5f49f3ef29
ocampott <ocampott7@gmail.com>
2022-08-02 11:50:39 -0300

    logo changes

--- apps/web/components/Logo.tsx
+++ apps/web/components/Logo.tsx
@@ -4,14 +4,19 @@ export default function Logo({ small, icon }: { small?: boolean; icon?: boolean 
       <strong>
         {icon ? (
           // eslint-disable-next-line @next/next/no-img-element
-          <img className="mx-auto w-9" alt="Cal" title="Cal" src="/cal-com-icon-white.svg" />
+          <img
+            className="mx-auto w-9"
+            alt="Logo #MarchettiRules"
+            title="Logo #MarchettiRules"
+            src="/LogoMR.png"
+          />
         ) : (
           // eslint-disable-next-line @next/next/no-img-element
           <img
-            className={small ? "h-4 w-auto" : "h-5 w-auto"}
-            alt="Cal"
-            title="Cal"
-            src="/calendso-logo-white-word.svg"
+            className={small ? "h-8 w-auto" : "h-7 w-auto"}
+            alt="Logo #MarchettiRules"
+            title="#MarchettiRules"
+            src="/LogoMR.png"
           />
         )}
       </strong>
